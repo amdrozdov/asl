4fee2136e287c19ce152b114d679237e03e6480a
a.drozdov <a.drozdov@criteo.com>
2023-09-18 13:29:27 +0200

    Linter pt.2

--- src/slice.cpp
+++ src/slice.cpp
@@ -1,18 +1,20 @@
+// Copyright 2023 Andrei Drozdov
+
+#include "./slice.h"  // NOLINT [build/include]
+
+#include <stdint.h>
 #include <string>
-#include "slice.h"
 #include <iostream>
 #include <vector>
 #include <cstdio>
 #include <cassert>
 #include <map>
 
-using namespace std;
-
-// codecs spec: https://www.signalogic.com/index.pl?page=audio_waveform_file_formats#table
+using namespace std;  // NOLINT [build/namespaces]
 
 
-void AudioSlicer::load_formats(){
-    this->format_mapping = map<short int, string>();
+void AudioSlicer::load_formats() {
+    this->format_mapping = map<int16_t, string>();
     format_mapping[0x1] = "Linear PCM";
     format_mapping[0x2] = "Microsoft ADPCM";
     format_mapping[0x3] = "IEEE floating-point";
@@ -32,36 +34,35 @@ void AudioSlicer::load_formats(){
     format_mapping[0x101] = "IBM M-LAW";
     format_mapping[0x102] = "IBM A-LAW";
     format_mapping[0x103] = "IBM ADPCM";
-
 }
 
-string AudioSlicer::audio_format(){
+string AudioSlicer::audio_format() {
     string res = std::to_string(this->header.AudioFormat) + " (";
     res += this->format_prefix + ")";
     return res;
 }
 
-void AudioSlicer::init(const string& fname){
+void AudioSlicer::init(const string& fname) {
     this->filename = fname;
     this->load_formats();
     this->read();
     this->is_verbose = false;
 }
 
-AudioSlicer::AudioSlicer(const string& fname){
+AudioSlicer::AudioSlicer(const string& fname) {
     this->init(fname);
 }
 
-AudioSlicer::AudioSlicer(const string& fname, bool is_verbose){
+AudioSlicer::AudioSlicer(const string& fname, bool is_verbose) {
     this->init(fname);
     this->is_verbose = is_verbose;
 }
 
-void AudioSlicer::read(){
+void AudioSlicer::read() {
     this->header = wav_header{};
 
     FILE* wavFile = fopen(this->filename.c_str(), "r");
-    if (wavFile == nullptr){
+    if (wavFile == nullptr) {
         cout << "Unable to read wave file: " << this->filename << endl;
     }
     assert(wavFile != nullptr);
@@ -70,25 +71,31 @@ void AudioSlicer::read(){
     assert(was_read);
     int bytes_per_sample = this->header.bitsPerSample / 8.0;
 
-    this->num_samples = double(header.Subchunk2Size) / double((header.NumOfChan) * bytes_per_sample);
+    this->num_samples = static_cast<double>(
+        header.Subchunk2Size) / static_cast<double>(
+        (header.NumOfChan) * bytes_per_sample);
     this->format_prefix = "Unknown";
-    if(this->format_mapping.find(header.AudioFormat) != this->format_mapping.end()){
+    if (this->format_mapping.find(
+            header.AudioFormat) != this->format_mapping.end()) {
         this->format_prefix = this->format_mapping[header.AudioFormat];
     }
-    this->duration = this->num_samples / double(this->header.SamplesPerSec);
+    this->duration = this->num_samples / static_cast<double>(
+        this->header.SamplesPerSec);
 
     this->channels = vector<char*>();
-    map<int,int> ch_pos = map<int,int>();
-    for(int i=0;i<this->header.NumOfChan;i++){
-        this->channels.push_back((char*)malloc(this->num_samples * bytes_per_sample));
+    map<int, int> ch_pos = map<int, int>();
+    for (int i=0; i < this->header.NumOfChan; i++) {
+        this->channels.push_back(
+            reinterpret_cast<char*>(malloc(
+                this->num_samples * bytes_per_sample)));
         ch_pos[i] = 0;
     }
-    char *buf = (char*)malloc(this->header.Subchunk2Size);
+    char *buf = reinterpret_cast<char*>(malloc(this->header.Subchunk2Size));
     size_t was_rd = fread(buf, 1, this->header.Subchunk2Size, wavFile);
     assert(was_rd);
 
     int ch = 0;
-    for(int i=0;i<this->header.Subchunk2Size;i+=bytes_per_sample){
+    for (int i=0; i < this->header.Subchunk2Size; i+=bytes_per_sample) {
         this->channels[ch][ch_pos[ch]++] = buf[i];
         this->channels[ch][ch_pos[ch]++] = buf[i+1];
         ch = (ch+1) % (this->channels.size());
@@ -98,7 +105,7 @@ void AudioSlicer::read(){
     fclose(wavFile);
 }
 
-void AudioSlicer::extract_audio(chunk slice){
+void AudioSlicer::extract_audio(chunk slice) {
     // Do not forget bitsPerSample
     // we need to write bps/8 bytes per sample
     int byte_per_sec = this->header.bitsPerSample / 8;
@@ -106,16 +113,16 @@ void AudioSlicer::extract_audio(chunk slice){
     int end = slice.sec_end * this->header.SamplesPerSec* byte_per_sec;
     int total_bytes = (end-start) * this->channels.size();
 
-    char * data = (char*)malloc(total_bytes);
+    char * data = reinterpret_cast<char*>(malloc(total_bytes));
     int p = 0;
 
     // wav format
     // [1b 1b] <- sample 1 ch 1, [1b 1b] sample 1 ch 2, ...
     // l11 l12 r11 r12 l21 l22 r21 r22
     int ptr = 0;
-    while(p<=total_bytes){
-        for (int i=0;i<this->channels.size();i++){
-            for(int b=0;b<byte_per_sec;b++){
+    while (p <= total_bytes) {
+        for (int i=0; i < this->channels.size(); i++) {
+            for (int b=0; b < byte_per_sec; b++) {
                 data[p++] = channels[i][start+ptr+b];
             }
         }
@@ -126,7 +133,7 @@ void AudioSlicer::extract_audio(chunk slice){
     new_header.Subchunk2Size = total_bytes;
 
     FILE* wavFile = fopen(slice.filename.c_str(), "wb");
-    if (wavFile == nullptr){
+    if (wavFile == nullptr) {
         cout << "Unable to read wave file: " << this->filename << endl;
     }
     assert(wavFile != nullptr);
@@ -135,21 +142,22 @@ void AudioSlicer::extract_audio(chunk slice){
     fclose(wavFile);
 
     free(data);
-    if(this->is_verbose){
-        cout << "Extracted interval [" << slice.sec_start<<":" << slice.sec_end << "] into '" << slice.filename << "'" << endl;
+    if (this->is_verbose) {
+        cout << "Extracted interval [" << slice.sec_start << ":";
+        cout << slice.sec_end << "] into '" << slice.filename << "'" << endl;
     }
 }
 
-void AudioSlicer::split_channels(string out_prefix){
+void AudioSlicer::split_channels(string out_prefix) {
     int byte_per_sec = this->header.bitsPerSample / 8;
-    for(int i=0;i<this->channels.size();i++){
+    for (int i=0; i < this->channels.size(); i++) {
         wav_header new_header = this->header;
         new_header.Subchunk2Size = this->num_samples * byte_per_sec;
         new_header.NumOfChan = 1;
 
         string fname = out_prefix + std::to_string(i) + ".wav";
         FILE* wavFile = fopen(fname.c_str(), "wb");
-        if (wavFile == nullptr){
+        if (wavFile == nullptr) {
             cout << "Unable to read wave file: " << this->filename << endl;
         }
         assert(wavFile != nullptr);
@@ -157,16 +165,17 @@ void AudioSlicer::split_channels(string out_prefix){
         fwrite(&new_header, 1, sizeof(new_header), wavFile);
         fwrite(this->channels[i], 1, this->num_samples * byte_per_sec, wavFile);
         fclose(wavFile);
-        if(this->is_verbose){
-            cout << "Extracted channel " << i << " into '" << out_prefix << i << ".wav'" << endl;
+        if (this->is_verbose) {
+            cout << "Extracted channel " << i << " into '";
+            cout << out_prefix << i << ".wav'" << endl;
         }
     }
 }
 
-void AudioSlicer::slice(vector<chunk> chunks){
-    for(int i=0;i<chunks.size();i++){
+void AudioSlicer::slice(vector<chunk> chunks) {
+    for (int i=0; i < chunks.size(); i++) {
         assert(chunks[i].sec_start >= 0);
-        assert(chunks[i].sec_end <= int(this->duration) + 1);
+        assert(chunks[i].sec_end <= static_cast<int>(this->duration) + 1);
         this->extract_audio(chunks[i]);
     }
 }
